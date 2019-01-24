04aee39bdca3f96606416e6ec8c6cfe0c6a3eaf3
SandmanPWNZ <thepomko@gmail.com>
2019-01-24 04:19:01 +0200

    name field validator fix

--- src/utils/FormValidate.js
+++ src/utils/FormValidate.js
@@ -1,6 +1,6 @@
 export const required = value => (value ? undefined : 'This field is required');
 export const num = value => (value && !/^[+0-9]/i.test(value) ? 'Only numbers allowed' : undefined);
-export const minLen = min => value => (value && value.length <= min ? undefined : `Minimum ${min} charecters`);
+export const minLen = min => value => (value && value.length >= min ? undefined : `Minimum ${min} charecters`);
 export const minLen3 = minLen(3);
 export const exactLen = len => value => (value && value.length === len ? undefined : 'This field should be exactly 9 charecters long');
 export const exactLen9 = exactLen(9);
